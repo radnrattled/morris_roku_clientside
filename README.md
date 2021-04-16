2a9f4a124fe80f8fcccb50809867b56d6473bcc0
Megan Morris <megs@Mac-mini.local>
2021-04-16 14:54:02 -0400

    .

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# morris_roku_clientside
+# morris_roku_clientside<br />
+<p align="center">
+  <a href="https://github.com/radnrattled/morris_roku_clientside">
+    <img src="images/roku.svg" alt="Logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">Megan Morris Roku Flaskback</h3>
+
+  <p align="center">
+   Building an audio / video app in the Authoring class, using queries and database
+assets. Creating assets based on the Roku Flashback brief (to be supplied) to create the layout, views and components you will be working with.
+</p>
+
+### Built With
+
+* HTML/CSS/JS
+* SASS/HANDLEBARS
+* VUE/NODE
+
+
+## Getting Started
+
+To get a local copy up and running follow these simple steps:
+* Clone the repo
+* git clone https://github.com/radnrattled/morris_roku_clientside
+* make sure you also have https://github.com/radnrattled/morris_roku_serverside
+* Open the folder
+* In a terminal window run:
+* npm run start
+* npm run start-dev
+* The app should open in your browser window using localhost.
+
+
+### Prerequisites
+
+* Vue and Node installed - localhost servers running.
+
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,13 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js';
-import TheMovieThumbs from './components/TheMovieThumbs.js';
+//import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js';
+//import TheMovieThumbs from './components/TheMovieThumbs.js';
+import LoginComponent from './components/TheLoginComponent.js';
+
+const router = new VueRouter({
+    routes: [
+        { path: "/", name: 'root', component: LoginComponent }
+    ]
+});
+
 (() => {
     const vm = new Vue({
         data: {
@@ -7,20 +15,21 @@ import TheMovieThumbs from './components/TheMovieThumbs.js';
         },
 
         created: function() {
-            fetch('./api/movies')
-            .then(res => res.json())
-            .then(data => {
-                console.table(data);
-                this.allMovies = data;
-            })
-            .catch(err => console.error(err));
+            //fetch('./api/movies')
+            //.then(res => res.json())
+            //.then(data => {
+              //  console.table(data);
+                //this.allMovies = data;
+            //})
+            //.catch(err => console.error(err));
         },
         methods: {
 
         },
         components: {
-            moviethumb: TheMovieThumbs
-        }
+           // moviethumb: TheMovieThumbs
+        },
 
+        router
     }).$mount('#app');
 })();
